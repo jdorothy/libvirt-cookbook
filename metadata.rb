a260b4cdb7db3e99594b4c3b84aab18d751d46aa
HIGUCHI Daisuke <d-higuchi@creationline.com>
2013-07-04 09:41:30 +0900

    support centos and rhel

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name		 "libvirt"
 maintainer       "VPSLab"
 maintainer_email "pawel.pacana@gmail.com"
 license          "Apache 2.0"
@@ -5,6 +6,6 @@ description      "Installs libvirt and provides libvirt resources."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.0"
 
-%w(ubuntu debian).each do |os|
+%w(ubuntu debian centos rhel).each do |os|
   supports os
 end
