bdb69744acf93acb115da9e31b74c742c75830dd
Josh Dorothy <joshua.dorothy@mtnsat.com>
2013-12-16 19:29:30 -0800

    Add additional options to disk_device template

--- providers/network_interface.rb
+++ providers/network_interface.rb
@@ -7,23 +7,25 @@ end
 action :attach do
   begin
     domain = load_domain
-    interface_xml = Tempfile.new(new_resource.name)
-    t = template interface_xml.path do
-      cookbook "libvirt"
-      source   "network_interface.xml"
-      variables(
-        :type   => new_resource.type,
-        :model  => new_resource.model,
-        :source => new_resource.source,
-        :domain => new_resource.domain,
-        :mac_address => new_resource.mac_address
-      )
-      action :nothing
-    end
-    t.run_action(:create)
+    unless domain.active?
+      interface_xml = Tempfile.new(new_resource.name)
+      t = template interface_xml.path do
+        cookbook "libvirt"
+        source   "network_interface.xml"
+        variables(
+          :type   => new_resource.type,
+          :model  => new_resource.model,
+          :source => new_resource.source,
+          :domain => new_resource.domain,
+          :mac_address => new_resource.mac_address
+        )
+        action :nothing
+      end
+      t.run_action(:create)
 
-    domain.attach_device(::File.read(interface_xml.path)) rescue nil
-    new_resource.updated_by_last_action(true)
+      domain.attach_device(::File.read(interface_xml.path)) rescue nil
+      new_resource.updated_by_last_action(true)
+    end
   rescue Libvirt::RetrieveError
     raise "You have to define libvirt domain '#{new_resource.domain}' first"
   end
--- resources/disk_device.rb
+++ resources/disk_device.rb
@@ -5,9 +5,11 @@ def initialize(*args)
   @action = :attach
 end
 
-attribute :type,   :kind_of => String, :default => 'block'
-attribute :bus,    :kind_of => String, :default => 'virtio'
-attribute :source, :kind_of => String
-attribute :target, :kind_of => String, :name_attribute => true
-attribute :domain, :kind_of => String
-attribute :uri,    :kind_of => String, :default => 'qemu:///system'
+attribute :type,          :kind_of => String, :default => 'block'
+attribute :driver,        :kind_of => String, :default => 'raw'
+attribute :bus,           :kind_of => String, :default => 'virtio'
+attribute :source_device, :kind_of => String, :default => 'file'
+attribute :source,        :kind_of => String
+attribute :target,        :kind_of => String, :name_attribute => true
+attribute :domain,        :kind_of => String
+attribute :uri,           :kind_of => String, :default => 'qemu:///system'
